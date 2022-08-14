4e347fb587e1960b10739dd39a5147eb2a9a9a11
Basantsingh787 <110412225+Basantsingh787@users.noreply.github.com>
2022-08-14 20:27:58 +0530

    word from

---
+++ word from as2.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#Write a Python program that accepts a word from the user and reverse it
+
+
+# In[1]:
+
+
+word = input("Input a word to reverse  = ")
+
+reversed_word=word[::-1]
+print(reversed_word)
+
+
+# In[ ]:
+
+
+
+
