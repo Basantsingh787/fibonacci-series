612a7faa77ff9790108b6658f64c29663394a92e
Basantsingh787 <110412225+Basantsingh787@users.noreply.github.com>
2022-09-04 23:14:36 +0530

    Write a Python program to create a lambda function that adds 25 to a given number passed in as an argument.

---
+++ Untitled13.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+###Write a Python program to create a lambda function that adds 25 to a given number passed in as an argument.
+#sample input: 10
+#sample output: 35
+
+
+# In[1]:
+
+
+n=int(input("enter number = "))
+x=lambda n:n+25
+print("after add result =" ,x(n))
+
+
+# In[ ]:
+
+
+
+
